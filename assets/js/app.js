26a3e4dbeb852dafd47240d77fd4c40a67ae8dc5
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-08 18:56:03 -0600

    fixed nav burger and made it sticky

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
+/////TYPEFORM/////
+(function () { var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName, id = "typef_orm_share", b = "https://embed.typeform.com/"; if (!gi.call(d, id)) { js = ce.call(d, "script"); js.id = id; js.src = b + "embed.js"; q = gt.call(d, "script")[0]; q.parentNode.insertBefore(js, q) } })() 
+
 let rsvpModal = window.typeformEmbed.makePopup("https://abigailjackson.typeform.com/to/T2U6Tu", {
         hideHeaders: true,
         hideFooter: true
-    })
+    });
 
+    // const sticky = UIkit.sticky('#navhamburger', {
+    //     offset: 50,
+    //     top: 100
+    //   });
 
-$("#rsvp-btn").on("click", function(){
-    rsvpModal.open();
-});
+    //   sticky;
  
